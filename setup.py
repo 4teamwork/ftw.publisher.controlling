9c1dd4f8bd7b26e3a898e363a61fb7839a6b95ba
Jonas Baumann <jone@jone.ch>
2010-05-12 08:20:48 +0000

    created package with paster (nested_namespace)

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = open('ftw/publisher/controlling/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
+
+setup(name='ftw.publisher.controlling',
+      version=version,
+      description="Controlling views for workflow bound ftw.publisher usage" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='ftw publisher controlling',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.publisher.controlling/',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', 'ftw.publisher'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
