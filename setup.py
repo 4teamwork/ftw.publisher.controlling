d3220fde94e54603d5a6994ebc3543e388efa2b4
Nick Espig <nickespig@gmail.com>
2019-11-04 16:40:47 +0100

    Setup plone 5.1 compatibility

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(name='ftw.publisher.controlling',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.1',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
